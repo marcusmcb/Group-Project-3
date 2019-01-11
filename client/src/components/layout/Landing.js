b04ade74f09f5fc876e6874470f6cc44d86e4307
Tanya Schwartz <38358730+tanyav112@users.noreply.github.com>
2019-01-10 19:15:39 -0800

    Added text to landing.js

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -19,7 +19,12 @@ class Landing extends Component {
                               <div className="row">
                                    <div className="col-md-12 text-center">
                                         <h1 className="display-3 mb-4">Eventagious</h1>
-                                        <p className="lead">A social media site for event professionals to connect and work with one another.</p>
+                                        <p className="lead">A social network and community for event professionals (photographers, DJS,
+security, videographers, etc) to connect and contract events (weddings,
+corporate events, trade shows, ect). Create an account with us to share your
+contact info, background experience and references. You can search for other
+professionals within our app, connect and communicate with one another to
+    book furture projects and events.</p>
                                         <hr />
                                         <Link to="/register" className="btn btn-lg btn-info mr-2">Sign Up</Link>
                                         <Link to="/login" className="btn btn-lg btn-light">Login</Link>
