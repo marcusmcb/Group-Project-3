a53c819faf716eda771f6ab7d362febf5a28a0b8
Marcus McBride <marcusmcb@gmail.com>
2019-01-10 19:29:33 -0800

    swapped out landing page background image

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -4,12 +4,12 @@ import { PropTypes } from 'prop-types';
 import { connect } from 'react-redux';
 
 class Landing extends Component {
-     
+
      componentDidMount() {
-     if(this.props.auth.isAuthenticated) {
-      this.props.history.push('/dashboard');
-    }
-  }
+          if (this.props.auth.isAuthenticated) {
+               this.props.history.push('/dashboard');
+          }
+     }
 
      render() {
           return (
@@ -19,11 +19,11 @@ class Landing extends Component {
                               <div className="row">
                                    <div className="col-md-12 text-center">
                                         <h1 className="display-3 mb-4">Eventagious</h1>
-                                        <p className="lead">A social network and community for event professionals (photographers, DJS,
-security, videographers, etc) to connect and contract events (weddings,
-corporate events, trade shows, ect). Create an account with us to share your
-contact info, background experience and references. You can search for other
-professionals within our app, connect and communicate with one another to
+                                        <p className="lead">A social network and community for event professionals (photographers, DJs, security, videographers, etc) to connect and contract events (weddings,
+corporate events, trade shows, ect).</p>
+                                        <p>Create an account with us to share your
+                                        contact info, background experience and references. You can search for other
+                                        professionals within our app, connect and communicate with one another to
     book furture projects and events.</p>
                                         <hr />
                                         <Link to="/register" className="btn btn-lg btn-info mr-2">Sign Up</Link>
