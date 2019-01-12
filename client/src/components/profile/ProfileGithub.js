4a31c5ca4d027f7449e7da6b60cf9fd0ab200cfa
Marcus McBride <marcusmcb@gmail.com>
2019-01-12 13:24:02 -0800

    added profile display components

---
+++ client/src/components/profile/ProfileGithub.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class ProfilGithub extends Component {
+     render() {
+          return (
+               <div>
+                    <h1>Profile Github:</h1>
+               </div>
+          )
+     }
+}
+
+export default ProfilGithub;
