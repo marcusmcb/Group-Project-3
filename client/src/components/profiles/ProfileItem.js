23300b500b4fe70f56cbab4897229a799cc1d984
hegratrix <gratrixmomma@yahoo.com>
2019-01-24 19:59:20 -0800

    profile

--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -16,7 +16,7 @@ class ProfileItem extends Component {
           <div className="col-lg-6 col-md-4 col-8">
             <h3>{profile.user.name}</h3>
             <p>
-              {profile.status}{' '}
+              {profile.profession}{' '}
               {isEmpty(profile.company) ? null : (
                 <span>at {profile.company}</span>
               )}
